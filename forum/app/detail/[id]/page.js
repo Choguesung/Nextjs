34fdb5929a9bea80c3c80bbcbf55257769dc234d
LSH <121619411+Choguesung@users.noreply.github.com>
2023-09-21 17:38:58 +0900

    021

--- forum/app/detail/[id]/page.js
+++ forum/app/detail/[id]/page.js
@@ -4,15 +4,19 @@ import Comment from "./comment"
 
 
 export default async function Detail(props) {
-    const db = (await connectDB).db("forum")
-    let result = await db.collection('post').findOne({ _id : new ObjectId(props.params.id)})
+  const db = (await connectDB).db("forum")
+  let result = await db.collection('post').findOne({ _id: new ObjectId(props.params.id) })
 
-    return (
-      <div>
-        <h4>글 상세 정보</h4>
-        <h4>{result.title}</h4>
-        <p>{result.content}</p>
-        <Comment _id={ result._id.toString()}/>
-      </div>
-    )
+  if (result == null) {
+    return <h4>404 에러임 그 페이지는 없는데요</h4>
+  } else {
+    return (<div>
+      <h4>글 상세 정보</h4>
+      <h4>{result.title}</h4>
+      <p>{result.content}</p>
+      <Comment _id={result._id.toString()} />
+    </div>)
   }
+
+
+}
