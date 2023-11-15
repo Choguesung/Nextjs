6a4125dde8eeeb9b98c3bb0ac6c48feddad63a86
LSH <121619411+Choguesung@users.noreply.github.com>
2023-11-15 22:23:45 +0900

    12

--- forum/app/cart/page.js
+++ forum/app/cart/page.js
@@ -1,17 +1,28 @@
+import { age } from "./data"
+
 export default function Cart() {
-    return (
-      <div>
-        <h4 className="title">Cart</h4>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-      </div>
-    )
-  }
+  let 장바구니 = ['Tomatoes', 'Pasta']
+  return (
+    <div>
+      <h4 className="title">Cart {age}</h4>
+      <CartItem 상품={장바구니[0]} />
+      <CartItem 상품={장바구니[1]} />
+    </div>
+  )
+}
+
+function Banner(){
+  return (
+    <h5>현대카드 결제 행사중</h5>
+  )
+} 
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.상품}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  )
+}
--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,14 +1,20 @@
+'use client'
+
+import { useState } from "react"
 
 export default function List() {
-    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
-    return (
-        상품.map((a, i)=>{
-            return (
-              <div className="food" key={i}>
-                <img src={`/food${i}.png`}/>
-                <h4>{a} $40</h4>
-              </div>
-            )
-        }) 
-    )
-  } 
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  let [수량, 수량변경] = useState(0)
+  return (
+    상품.map((a, i) => {
+      return (
+        <div className="food" key={i}>
+          <img src={`/food${i}.png`} />
+          <h4>{a} $40</h4>
+          <span> {수량} </span>
+          <button onClick={()=>{ 수량변경(수량+1) }}>+</button> 
+        </div>
+      )
+    })
+  )
+} 
