058c9851716705d5a5616dbf0a49e1c75e415128
LSH <121619411+Choguesung@users.noreply.github.com>
2023-11-18 20:02:17 +0900

    a

--- notice/app/detail/[id]/page.js
+++ notice/app/detail/[id]/page.js
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongodb";
 import { connectDB } from "@/util/database.js"
+import Comment from "./comment";
 
 export default async function Detail(props) {
   let db = (await connectDB).db('forum')
@@ -11,6 +12,7 @@ export default async function Detail(props) {
       <h4>상세페이지임</h4>
       <h4>{result.title}</h4>
       <p>{result.content}</p>
+      <Comment _id={ result._id.toString() }/> 
     </div>
   )
 }
