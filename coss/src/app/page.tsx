ff44d788505ce364f32df4a350f7d85696ccb5f2
LSH <121619411+Choguesung@users.noreply.github.com>
2024-01-01 15:50:55 +0900

    add:

---
+++ coss/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+       <div>안녕안녕 나는 </div>
+       <div>안녕안녕 나는 </div>
+    </main>
+  )
+}
